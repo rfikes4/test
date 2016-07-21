86a345cdfc4f46dd17adcd7d9a074bc5ba703d1b
rfikes4 <robertfikesiv@Roberts-MacBook-Pro.local>
2016-07-21 15:45:18 -0600

    modified for osc

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
+
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
@@ -19,3 +20,22 @@ io.on('connection', (socket) => {
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+
+
+
+
+
+
+var osc       =     require('node-osc');
+var client 	= 		new osc.Client('127.0.0.1', PORT);
+// ========== Data ========== //
+//Sends OSC message when button is clicked
+io.sockets.on('connection', function(socket){
+  socket.on('send message', function(data){
+    var oscNum = Math.random();
+    var oscMap = '/composition/video/effect3/opacity/values ' + oscNum;
+
+    client.send('/composition/video/effect3/opacity/values', oscNum);
+    console.log(oscMap);
+  });
+});
